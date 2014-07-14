d0263775cc1bfc0bdb8ba7e0253bfe8fb4b7853c
정 다정 <dajeong@jeong-ui-MacBook-Pro.local>
2014-07-14 16:48:25 +0900

    수정중
     수정중

--- iStoryBall/Controller/Menu/MenuViewController.swift
+++ iStoryBall/Controller/Menu/MenuViewController.swift
@@ -110,7 +110,7 @@ class MenuViewController : SBViewController, UITableViewDelegate, UITableViewDat
             println(indexPath.row)
         case 7: // 스토리볼 공지
             println(indexPath.row)
-            // 다시 테이블 뷰 컨트롤러로 만들어야 한다!
+            // 다시 테이블 뷰 컨트롤러를 만들어야 한다!
             
         case 8: // 스토리볼 연재 제안
             println(indexPath.row)
