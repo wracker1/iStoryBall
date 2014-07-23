2a88c18aae074e08661f170b81d168ef055e249f
darm <gyul6@hanmail.net>
2014-07-23 14:20:08 +0900

    맞춤형 스토리에 이미지 추가

--- iStoryBall/Controller/ChoiceViewDetailController.swift
+++ iStoryBall/Controller/ChoiceViewDetailController.swift
@@ -32,13 +32,29 @@ class ChoiceViewDetailController: SBViewController{
         var recommend:TFHppleElement = self.doc?.itemsWithQuery(".link_recomm")[0] as TFHppleElement
         var imageElement:TFHppleElement = recommend.itemsWithQuery(".thumb_img")[0] as TFHppleElement
 
-        var image = imageElement.imageUrlFromHppleElement()
+        var imageUrl = imageElement.imageUrlFromHppleElement()
         var title = recommend.attributes["title"] as NSString
 
         var label = UILabel()
         label.frame = CGRectMake(0, 0, 320, 70)
         label.text = title
+        label.setTranslatesAutoresizingMaskIntoConstraints(false)
         self.view.addSubview(label)
+        
+        var image = UIImage(data: NSData(contentsOfURL: NSURL(string:imageUrl)))
+        var imageView = UIImageView(image: image)
+        imageView.contentMode = UIViewContentMode.ScaleAspectFit
+        imageView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.view.addSubview(imageView)
+        
+        var labelHConst = NSLayoutConstraint.constraintsWithVisualFormat("H:|-[label(>=200)]-|", options: NSLayoutFormatOptions(0), metrics: nil, views: ["label": label])
+        var imageHConst = NSLayoutConstraint.constraintsWithVisualFormat("H:|-[image(>=200)]-|", options: NSLayoutFormatOptions(0), metrics: nil, views: ["image": imageView])
+        var vConst = NSLayoutConstraint.constraintsWithVisualFormat("V:|-[label(70)]-[image(>=100)]-|", options: NSLayoutFormatOptions(0), metrics: nil, views: ["label": label, "image": imageView])
+        
+        self.view.addConstraints(labelHConst)
+        self.view.addConstraints(imageHConst)
+        self.view.addConstraints(vConst)
+        
     }
     
 }
