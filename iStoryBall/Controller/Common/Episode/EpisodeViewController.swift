40e9c42626d4fc8f8c115ff6989e4dbfc3bd30ab
이동현 <wracker1@daumcorp.com>
2014-07-24 18:33:06 +0900

    개발

--- iStoryBall/Controller/Common/Episode/EpisodeViewController.swift
+++ iStoryBall/Controller/Common/Episode/EpisodeViewController.swift
@@ -8,9 +8,37 @@
 
 class EpisodeViewController: SBViewController
 {
+    var doc: TFHpple?
+//    var html: String?
+    var contentWebview: UIWebView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if let episodeId = id {
+            NetClient.instance.get(episodeId) {
+                (html: String) in
+//                self.html = html
+                self.doc = html.htmlDocument()
+                self.layoutSubviews()
+            }
+        }
+    }
+    
+    func layoutSubviews() {
+        createContentWebview()
+    }
+    
+    func createContentWebview() {
+        var articleBody = doc!.itemWithQuery("#viewBody")
+        var head = doc!.itemWithQuery("head").raw.trim()
+        var body = articleBody.raw.trim()
+        var html = "<!DOCTYPE html>\(head)<body data-ctpageid=\"storyballView\"><div class=\"cont_view bnr_on fit\" id=\"daumWrap\"><article id=\"daumContent\"><div id=\"cMain\"><article id=\"mArticle\" role=\"main\">\(body)</article></div></article></div></body>"
+        var size = self.view.bounds.size
         
+        contentWebview = UIWebView(frame: CGRect(origin: CGPointZero, size: size))
+        self.view.addSubview(contentWebview)
+        contentWebview!.layoutTopInParentView()
+        contentWebview!.loadHTMLString(html, baseURL: NetClient.instance.manager?.baseURL)
     }
 }
