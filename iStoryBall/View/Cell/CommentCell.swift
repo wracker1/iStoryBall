5c664345f2fc767d221ed3e583e050f85af54ef4
이동현 <wracker1@daumcorp.com>
2014-07-29 17:50:53 +0900

    댓글셀 추

---
+++ iStoryBall/View/Cell/CommentCell.swift
@@ -0,0 +1,12 @@
+//
+//  CommentCell.swift
+//  iStoryBall
+//
+//  Created by Jesse on 2014. 7. 29..
+//  Copyright (c) 2014년 Daum communications. All rights reserved.
+//
+
+class CommentCell: SBTableViewCell
+{
+    
+}
